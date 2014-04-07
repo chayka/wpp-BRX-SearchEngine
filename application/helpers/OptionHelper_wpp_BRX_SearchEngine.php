f5b249cf1da0ce525700281e69c54d6afc569587
Boris Mossounov <borix@tut.by>
2014-04-07 14:32:46 +0300

    Minor fixes

--- application/helpers/OptionHelper_wpp_BRX_SearchEngine.php
+++ application/helpers/OptionHelper_wpp_BRX_SearchEngine.php
@@ -14,10 +14,20 @@ class OptionHelper_wpp_BRX_SearchEngine {
 
     public static function getOption($option, $default='', $reload = false){
         $key = 'wpp_BRX_SearchEngine.'.$option;
-        return get_site_option($key, $default, !$reload);
+        return get_option($key, $default, !$reload);
     }
     
     public static function setOption($option, $value){
+        $key = 'wpp_BRX_SearchEngine.'.$option;
+        return update_option($key, $value);
+    }
+    
+    public static function getSiteOption($option, $default='', $reload = false){
+        $key = 'wpp_BRX_SearchEngine.'.$option;
+        return get_site_option($key, $default, !$reload);
+    }
+    
+    public static function setSiteOption($option, $value){
         $key = 'wpp_BRX_SearchEngine.'.$option;
         return update_site_option($key, $value);
     }
